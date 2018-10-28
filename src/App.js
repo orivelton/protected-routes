ad2fd50fee29e704becdcb70cf0c07b21c7f33b4
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-10-28 16:11:09 -0300

    Add router and react router protected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { LeadingPage } from './components/leading.page';
+import { AppLayout } from './components/app.layout'
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Protected React Router</h1>
+        <Route path='/' exact component={LeadingPage}/>
+        <Route path='/app' exact component={AppLayout}/>
       </div>
     );
   }
--- src/components/leading.page.js
+++ src/components/leading.page.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import auth from '../auth/auth';
 
-export const LeadingPage = () => {
-  return <div>Leading Page</div>
+export const LeadingPage = props => {
+  return (
+    <div>
+      <h2>Leading Page</h2>
+      <button onClick={
+        () => {
+          auth.login(() => {
+            props.history.push('/app');
+          })
+        }
+      }>Login</button>
+    </div>
+  )
 }
